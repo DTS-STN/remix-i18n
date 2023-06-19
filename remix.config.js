4144f69574f3f12c698f0310a2e3706f4feb515b
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2023-06-19 08:42:00 -0230

    Bump dependencies

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   future: {
-    unstable_tailwind: true,
+    v2_headers: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
+  serverModuleFormat: 'cjs',
+  tailwind: true,
 };
