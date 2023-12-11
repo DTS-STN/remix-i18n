b4aecee9d4a41169e4db3806892e702cdc5d16ac
frank basham <frankabasham@gmail.com>
2023-12-11 08:50:46 -0500

    null coallesce lang attr and expand random background amount

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,7 @@ const App: FC = () => {
   const { locale } = useLoaderData<typeof loader>();
 
   return (
-    <html lang={locale}>
+    <html lang={locale ?? 'en'}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
